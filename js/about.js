bb0a956c617476ea32ae1ba7c4910dd5c8252e73
Nicholas Doelger <nicholasdoelder@9122-L-A-MBP1621.local>
2024-08-08 15:53:37 -0400

    sliding panels

--- js/about.js
+++ js/about.js
@@ -1,29 +1,23 @@
 gsap.registerPlugin(ScrollTrigger);
 
 document.addEventListener("DOMContentLoaded", (event) => {
-  
-  // const tl = gsap.timeline({
-  //   scrollTrigger: {
-  //     trigger: ".panel",
-  //     markers: true,
-  //     scrub: true,
-  //     start: "top bottom",
-  //     end: "bottom bottom",
-  //   },
-  // });
+    const totalPanels = document.querySelectorAll(".panel").length;
+    const totalScrollDuration = window.innerHeight * totalPanels; // 300vh
+    const staggerTime = totalScrollDuration / totalPanels; // duration per panel
 
-  gsap.fromTo(
-    ".panel",
-    { x: window.innerWidth },
-    {
-      x: 0,
-      scrollTrigger: {
-        trigger: ".letter",
-        scrub: true,
-        start: `top top`,
-        end: "bottom 100%",
-        markers: true,
-      },
-    }
-  );
+    gsap.set(".panel", { x: window.innerWidth });
+
+    gsap.to(".panel", {
+        x: 0,
+        stagger: {
+            each: staggerTime / 1000, // Convert to seconds for GSAP stagger
+        },
+        scrollTrigger: {
+            trigger: ".panel",
+            scrub: true,
+            start: "top top",
+            end: `+=${totalScrollDuration}px`, // Total scroll duration
+            markers: true,
+        },
+    });
 });
